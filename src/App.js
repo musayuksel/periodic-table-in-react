1b4ba2ee98061c6487c5b892544b9684e6700ac6
mrmikeshields-cyf <mr.m.p.shields@gmail.com>
2021-09-02 19:14:37 +0100

    feat: fetch element data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
 function App() {
+  
+  fetch("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json")
+    .then(response => response.json())
+    .then(data => console.log(data))
+  
   return (
     <div className="App">
-      
+
     </div>
   );
 }
